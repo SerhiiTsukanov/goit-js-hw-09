91579df652cc1adcdc710767abb2526d3074dcac
SerhiiTsukanov <94555214+SerhiiTsukanov@users.noreply.github.com>
2022-08-12 14:28:46 +0000

    Deploying to gh-pages from @ SerhiiTsukanov/goit-js-hw-09@296819131270082cac6cbdf2e395b7333e0d9ae4 🚀

---
+++ 01-color-switcher.85d77ed0.js
@@ -0,0 +1,2 @@
+dataStart=document.querySelector("button, [data-start]"),dataStop=document.querySelector("button, [data-stop]"),body=document.querySelector("body"),console.log(dataStop),button.addEventListener("button");
+//# sourceMappingURL=01-color-switcher.85d77ed0.js.map
---
+++ 01-color-switcher.d41ad6d6.js
@@ -0,0 +1,2 @@
+dataStart=document.querySelector("button, [data-start]"),dataStop=document.querySelector("button, [data-stop]"),body=document.querySelector("body"),console.log(dataStop),button.addEventListener("button");
+//# sourceMappingURL=01-color-switcher.d41ad6d6.js.map
