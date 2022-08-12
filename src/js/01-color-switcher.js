296819131270082cac6cbdf2e395b7333e0d9ae4
SerhiiTsukanov <94555214+SerhiiTsukanov@users.noreply.github.com>
2022-08-12 17:28:14 +0300

    querySelector

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,12 @@
+// import '../css/common.css'
 
+dataStart = document.querySelector('button, [data-start]');
+dataStop = document.querySelector('button, [data-stop]');
+body = document.querySelector('body');
+console.log(dataStop)
+
+button.addEventListener("button", )
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
