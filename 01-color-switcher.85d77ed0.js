e2148246800daeb8ff973beb60fe55093d8c7669
SerhiiTsukanov <94555214+SerhiiTsukanov@users.noreply.github.com>
2022-08-13 09:07:48 +0000

    Deploying to gh-pages from @ SerhiiTsukanov/goit-js-hw-09@92662b85d18308121e8b0eb9105b89af36ded083 🚀

--- 01-color-switcher.85d77ed0.js
+++
@@ -1,2 +0,0 @@
-dataStart=document.querySelector("button, [data-start]"),dataStop=document.querySelector("button, [data-stop]"),body=document.querySelector("body"),console.log(dataStop),button.addEventListener("button");
-//# sourceMappingURL=01-color-switcher.85d77ed0.js.map
