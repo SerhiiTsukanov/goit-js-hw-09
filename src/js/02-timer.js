37a1e741a201ab1ff21f67cc57426b3df23dd166
SerhiiTsukanov <94555214+SerhiiTsukanov@users.noreply.github.com>
2022-08-17 01:12:58 +0300

    333

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,18 +1,17 @@
-import '../css/common.css';
-// const flatpickr = require("flatpickr");
+
+import Notiflix from 'notiflix';
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
-import Notiflix from 'notiflix';
+import '../css/common.css';
 
-inputFlatpickr = document.querySelector("#datetime-picker");
-startBtn = document.querySelector('button[data-start]');
+inputFlatpickr = document.querySelector("input#datetime-picker");
+startBtn = document.querySelector("button[data-start]");
 valueDays = document.querySelector('span[data-days]');
 valueHours = document.querySelector('span[data-hours]');
 valueMinutes = document.querySelector('span[data-minutes]');
 valueSeconds = document.querySelector('span[data-seconds]');
 startBtn.disabled = true;
-flatpickr(inputFlatpickr, options); 
-
+ 
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -20,7 +19,7 @@ const options = {
   minuteIncrement: 1,
     onClose(selectedDates) {
       if (selectedDates[0] <= new Date()) {
-        document.querySelector('button[data-start]').disabled = true;
+        document.querySelector("button[data-start]").disabled = true;
         clearInterval(intervalID);
          Notiflix.Notify.warning('Please choose a date in the future');
          return;
@@ -36,15 +35,14 @@ const options = {
           document.querySelector('button[data-start]').disabled = true;
           if (delta <= 10) {
             clearInterval(intervalID);
-            // if (daysElement.textContent === 0 && hoursElement.textContent === 0 && minutesElement.textContent === 0 && secondsElement.textContent === 0) {
-            //     clearInterval(intrvalId)
-            // }
             return;
           }
         }
     console.log(selectedDates[0]);
   },
 };
+
+flatpickr(inputFlatpickr, options);
 
 // Для подсчета значений
 function convertMs(ms) {
@@ -66,14 +64,15 @@ function convertMs(ms) {
   return { days, hours, minutes, seconds };
 }
 
-// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
 
-// console.log(valueDays.textContent);
-// console.log(valueHours.textContent);
-// console.log(valueMinutes.textContent);
-// console.log(valueSeconds.textContent);
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+console.log(valueDays.textContent);
+console.log(valueHours.textContent);
+console.log(valueMinutes.textContent);
+console.log(valueSeconds.textContent);
 
 // Форматирование времени
 function addLeadingZero(value) {
