22d7317146d5fb23c6137f3f88da080de80a5b03
SerhiiTsukanov <94555214+SerhiiTsukanov@users.noreply.github.com>
2022-08-17 16:31:11 +0300

    new

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,31 +1,29 @@
-
-import Notiflix from 'notiflix';
+// import '../css/common.css';
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
-import '../css/common.css';
+import Notiflix from 'notiflix';
 
 inputFlatpickr = document.querySelector('#datetime-picker');
-startBtn = document.querySelector("button[data-start]");
+startBtn = document.querySelector('button[data-start]');
 valueDays = document.querySelector('span[data-days]');
 valueHours = document.querySelector('span[data-hours]');
 valueMinutes = document.querySelector('span[data-minutes]');
 valueSeconds = document.querySelector('span[data-seconds]');
 
-startBtn.disabled = true;
- 
+
+document.querySelector('[data-start]').disabled = true;
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-    onClose(selectedDates) {
-      if (selectedDates[0] <= new Date()) {
-        document.querySelector("button[data-start]").disabled = true;
-        clearInterval(intervalID);
+  onClose(selectedDates) {
+     if (selectedDates[0] <= new Date()) {
          Notiflix.Notify.warning('Please choose a date in the future');
          return;
         } 
-        startBtn.disabled = false;
+        document.querySelector('[data-start]').disabled = false;
         startBtn.addEventListener('click', timer);
         function timer() {
             const intervalID = setInterval(() => {
@@ -33,17 +31,13 @@ const options = {
                 const delta = chooiceData - currentData;
                 convertMs(delta);
             }, 1000)
-          document.querySelector('button[data-start]').disabled = true;
-          if (delta <= 10) {
-            clearInterval(intervalID);
-            return;
-          }
+            // if () {clearInterval(intervalID)}
         }
     console.log(selectedDates[0]);
   },
 };
 
-flatpickr(inputFlatpickr, options);
+flatpickr(inputFlatpickr, options); 
 
 // Для подсчета значений
 function convertMs(ms) {
@@ -65,28 +59,15 @@ function convertMs(ms) {
   return { days, hours, minutes, seconds };
 }
 
-
 console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
 console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
 console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
 
-console.log(valueDays.textContent);
-console.log(valueHours.textContent);
-console.log(valueMinutes.textContent);
-console.log(valueSeconds.textContent);
-
 // Форматирование времени
 function addLeadingZero(value) {
-  return String(value).startBtn(2, '0');
+
 }
 
 // const timer = {
 //     start() {
 //         const startTime = Data.now();
-
-//         setInterval(() => {
-//             console.log('ssss')
-//             // const currentTime = Data.now();
-//         }, 1000);
-//     }
-// }
