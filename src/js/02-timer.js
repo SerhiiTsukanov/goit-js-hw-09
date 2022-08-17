ba98fd4d51230cd377b0442de3de4fbdb4a1a750
SerhiiTsukanov <94555214+SerhiiTsukanov@users.noreply.github.com>
2022-08-18 01:22:19 +0300

    5555

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -19,18 +19,33 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
+    
      if (selectedDates[0] <= new Date()) {
          Notiflix.Notify.warning('Please choose a date in the future');
          return;
         } 
         document.querySelector('[data-start]').disabled = false;
         startBtn.addEventListener('click', timer);
-        function timer() {
-            const intervalID = setInterval(() => {
-                const currentData = Data.now();
-                const delta = chooiceData - currentData;
-                convertMs(delta);
-            }, 1000)
+    function timer() {
+      // intervalID: null,
+      // this.isActive = true;
+          
+      document.querySelector('button[data-start]').disabled = true;
+      // if (isActive) {
+      //   return;
+      // }
+      const intervalID = setInterval(() => {
+        
+        // if (delta <= 100) {
+        //   clearInterval(intervalID);
+        //   return;
+        // }
+        const currentDate = Date.now();
+          const delta = selectedDates - currentDate;
+        
+        addLeadingZero(convertMs(delta));
+      }, 1000)
+      
             // if () {clearInterval(intervalID)}
         }
     console.log(selectedDates[0]);
@@ -38,6 +53,11 @@ const options = {
 };
 
 flatpickr(inputFlatpickr, options); 
+
+// Добавляет 0
+function pad(value) {
+  return String(value).padStart(2, 0);
+}
 
 // Для подсчета значений
 function convertMs(ms) {
@@ -48,13 +68,13 @@ function convertMs(ms) {
   const day = hour * 24;
 
   // Remaining days
-  const days = Math.floor(ms / day);
+  const days = pad(Math.floor(ms / day));
   // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
+  const hours = pad(Math.floor((ms % day) / hour));
   // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const minutes = pad(Math.floor(((ms % day) % hour) / minute));
   // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
 
   return { days, hours, minutes, seconds };
 }
@@ -63,9 +83,14 @@ console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
 console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
 console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
 
-// Форматирование времени
-function addLeadingZero(value) {
 
+
+// Форматирование времени
+function addLeadingZero({ days, hours, minutes, seconds }) {
+valueDays.textContent = days;
+valueHours.textContent = hours;
+valueMinutes.textContent = minutes;
+valueSeconds.textContent = seconds;
 }
 
 // const timer = {
