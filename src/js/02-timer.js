c6c004b65a2e1a3dac3fdc8d50e350ce0d86c91f
SerhiiTsukanov <94555214+SerhiiTsukanov@users.noreply.github.com>
2022-08-21 00:07:38 +0300

    fff

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,94 +1,111 @@
-// import '../css/common.css';
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 import Notiflix from 'notiflix';
+import "notiflix/dist/notiflix-3.2.5.min.css";
 
-inputFlatpickr = document.querySelector('#datetime-picker');
-startBtn = document.querySelector('button[data-start]');
-valueDays = document.querySelector('span[data-days]');
-valueHours = document.querySelector('span[data-hours]');
-valueMinutes = document.querySelector('span[data-minutes]');
-valueSeconds = document.querySelector('span[data-seconds]');
+const refs = {
+    startBtn: document.querySelector('button[data-start]'),
+    clockfaceDays: document.querySelector('[data-days]'),
+    clockfaceHours: document.querySelector('[data-hours]'),
+    clockfaceMinutes: document.querySelector('[data-minutes]'),
+    clockfaceSeconds: document.querySelector('[data-seconds]'),
+    flatpickrSelect: document.querySelector('input[type="text"]'),
+  }
 
-document.querySelector('[data-start]').disabled = true;
+
+let choseDate = [];
+const currentDate = new Date();
+
+refs.startBtn.setAttribute('disabled', true);
 
 const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    if (selectedDates[0] <= new Date()) {
-      Notiflix.Notify.warning('Please choose a date in the future');
-      return;
-    } 
-      document.querySelector('[data-start]').disabled = false;
-      startBtn.addEventListener('click', timer);
-    
-    console.log(selectedDates[0]);
-  }
-};
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+      if (selectedDates[0] < currentDate) {
+        Notiflix.Notify.warning('Please choose a date in the future!');
+      }
+      choseDate = selectedDates[0];
+      refs.startBtn.removeAttribute('disabled', true);
+      refs.startBtn.addEventListener('click', timer.start.bind(timer));
+    },
+  };
 
-function timer() {
-  intervalID = null;
-      // this.isActive = true;
-  document.querySelector('button[data-start]').disabled = true;
-      // if (isActive) {
-      //   return;
-      // }
+flatpickr(refs.flatpickrSelect, options);
+
+  class Timer {
+    constructor({ onTick }) {
+      this.intervalId = null;
+      this.isActive = false;
+      this.onTick = onTick;
   
+      this.init();
+    }
   
-  intervalID = setInterval(() => { 
-    
-    const currentDate = Date.now();
-    const delta = selectedDates - currentDate;
-    convertMs(delta);
-  }, 1000);
-  if (delta <= 100) {
-    clearInterval(intervalID);
+    init() {
+      const time = this.convertMs(0);
+      this.onTick(time);
+    }
+  
+    start() {
+      if (this.isActive) {
+        return;
+      }
+      this.isActive = true;
+      this.changeBodyColor()
+      this.intervalId = setInterval(() => {
+         this.changeBodyColor()
+
+      }, 1000);
+    }
+   convertMs(ms) {
+        // Number of milliseconds per unit of time
+        const second = 1000;
+        const minute = second * 60;
+        const hour = minute * 60;
+        const day = hour * 24;
+      
+        // Remaining days
+        const days = this.pad(Math.floor(ms / day));
+        // Remaining hours
+        const hours = this.pad(Math.floor((ms % day) / hour));
+        // Remaining minutes
+        const minutes = this.pad(Math.floor(((ms % day) % hour) / minute));
+        // Remaining seconds
+        const seconds = this.pad(Math.floor((((ms % day) % hour) % minute) / second));
+      
+        return { days, hours, minutes, seconds };
+      }
+    // Добавляет к часам минутам и секундам нолик
+    pad(value) {
+      return String(value).padStart(2, "0");
+    }
+    changeBodyColor() {
+    const currentTime = new Date();
+    const deltaTime = choseDate - currentTime;
+    const time = this.convertMs(deltaTime);
+    this.onTick(time);
+    refs.startBtn.setAttribute('disabled', true);
+    if (deltaTime <= 1000){
+      clearInterval(this.intervalId);
+      refs.startBtn.removeAttribute('disabled', true);
       return;
     }
-};
+  }
+  }
+  
+  const timer = new Timer({
+    onTick: updateClockface,
+  });
+  
+  refs.startBtn.addEventListener("click", timer.start.bind(timer));
 
-flatpickr(inputFlatpickr, options); 
-
-// Добавляет 0
-function addLeadingZero(value) {
-  return String(value).padStart(2, 0);
-}
-
-// Для подсчета значений
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  // Remaining days
-  const days = addLeadingZero(Math.floor(ms / day));
-  // Remaining hours
-  const hours = addLeadingZero(Math.floor((ms % day) / hour));
-  // Remaining minutes
-  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
-  // Remaining seconds
-  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
-
-  return { days, hours, minutes, seconds };
-}
-
-console.log(convertMs(days)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(hours)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(minutes)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
-
-// Форматирование времени
-function updateClockface({ days, hours, minutes, seconds }) {
-valueDays.textContent = `${days}`;
-valueHours.textContent = `${hours}`;
-valueMinutes.textContent = `${minutes}`;
-valueSeconds.textContent = `${seconds}`;
-}
-
-// const timer = {
-//     start() {
-//         const startTime = Data.now();
+  
+  function updateClockface({ days, hours, minutes, seconds }) {
+    refs.clockfaceDays.textContent = `${days}:`;
+    refs.clockfaceHours.textContent = `${hours}:`;
+    refs.clockfaceMinutes.textContent = `${minutes}:`;
+    refs.clockfaceSeconds.textContent = `${seconds}`;
+  }
