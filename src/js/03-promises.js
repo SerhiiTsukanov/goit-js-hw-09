6c3d53ba4309556a7d0f777aef2241e12c4b4ede
SerhiiTsukanov <94555214+SerhiiTsukanov@users.noreply.github.com>
2022-08-23 15:09:04 +0300

    start

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,4 +1,15 @@
-function createPromise(position, delay) {
+import Notiflix from 'notiflix';
+import "notiflix/dist/notiflix-3.2.5.min.css";
+
+const refs = {
+  submitBtn: document.querySelector('button[submit]')
+}
+
+refs.submitBtn.addEventListener('click', createPromise);
+
+const promise = new Promise((resolve, reject) => createPromise);
+  
+ function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
     // Fulfill
@@ -6,3 +17,16 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+
+
+promise(2, 1500)
+  .then(({ position, delay }) => {
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+
+  
+  // Asynchronous operation
