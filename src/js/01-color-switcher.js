5dda7cd8721041c5816aaf98f00e50fc296d8f09
SerhiiTsukanov <94555214+SerhiiTsukanov@users.noreply.github.com>
2022-09-04 23:05:44 +0300

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,15 +1,11 @@
 
-
 const startBtn = document.querySelector('[data-start]');
-const stopBtn = document.querySelector('button[data-stop]');
-
-console.log(startBtn);
+const stopBtn = document.querySelector('[data-stop]');
 
 document.querySelector('[data-stop]').disabled = true;
 
 function changeBackground() {
   document.body.style.backgroundColor = getRandomHexColor();
-  console.log('измен фона', changeBackground);
 }
 
 function getRandomHexColor() {
@@ -17,10 +13,12 @@ function getRandomHexColor() {
 }
 
 startBtn.addEventListener('click', timerChangeBackground);
+
 function timerChangeBackground() {
   timerId = setInterval(changeBackground, 1000);
   document.querySelector('[data-start]').disabled = true;
   document.querySelector('[data-stop]').disabled = false;
+  startBtn.removeEventListener('click', timerChangeBackground);
   stopBtn.addEventListener('click', removeEventListenerFromStopBtn)
 };
 
@@ -28,6 +26,6 @@ function removeEventListenerFromStopBtn() {
   clearInterval(timerId);
   document.querySelector('[data-start]').disabled = false;
   document.querySelector('[data-stop]').disabled = true;
-  stopBtn.removeEventListener('click',removeEventListenerFromStopBtn);
+  stopBtn.removeEventListener('click', removeEventListenerFromStopBtn);
+  startBtn.addEventListener('click', timerChangeBackground);
 }
-
